303ff512209f6e04e966fac149f93625c49298af
JoelCruzCode <136981207+JoelCruzCode@users.noreply.github.com>
2023-07-08 21:29:52 -0700

    create dom elements & methods to display ui

--- script.js
+++ script.js
@@ -1,45 +1,104 @@
+// TO DO LIST
+// 1.) Refactor css/html and use query selector for buttons
+// 2.) find out why  game isn't ending with conditional of 5pts
+// 3.) why does innerhtml/textcontent work when being applied to queryselector but not getelementbyclassname
+
 let pScore = 0;
 let cScore = 0;
+const player = document.querySelector(".player");
+const computer = document.querySelector(".computer");
 const choices = ["Rock", "Paper", "Scissors"];
+const rock = document.querySelector("rock");
+const paper = document.querySelector("paper");
+const scissors = document.querySelector("scissors");
+const buttons = document.querySelectorAll("button");
+
+const updateScores = function () {
+  player.textContent = `Player Score = ${pScore}`;
+  computer.textContent = `Computer score = ${cScore}`;
+};
 
 const getComputerChoice = function () {
   return choices[Math.trunc(Math.random() * choices.length)];
 };
 
-const playRound = function (playerChoice, computerChoice) {
+const playRound = function () {
+  const computerChoice = getComputerChoice();
   const pc =
-    playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase();
+    this.textContent[0].toUpperCase() + this.textContent.slice(1).toLowerCase();
   console.log(`You chose ${pc}`);
   console.log(`Computer chose ${computerChoice}`);
   if (pc === computerChoice) {
-    return "Its a Draw!";
+    console.log("Its a Draw");
+    // return "Its a Draw!";
   } else if (
     (pc === "Rock" && computerChoice === "Paper") ||
     (pc === "Paper" && computerChoice === "Scissors") ||
     (pc === "Scissors" && computerChoice === "Rock")
   ) {
     cScore++;
-    return "You Lose!";
+    updateScores();
+    // return "You Lose!";
   } else {
     pScore++;
-    return "You Win!";
+    updateScores();
+    // return "You Win!";
   }
 };
 
-const game = function () {
-  for (i = 0; i < 5; i++) {
-    const playerChoice = prompt('Choose between "Rock", "Paper" or "Scissors"');
-    const computerChoice = getComputerChoice();
-    console.log(playRound(playerChoice, computerChoice));
-    console.log(`Current score is player: ${pScore} | Computer: ${cScore}`);
-  }
-  if (pScore === cScore) {
-    console.log("*****Game over, Its a Draw!*****");
-  } else if (pScore < cScore) {
-    console.log("****Game over, you lose!*****");
-  } else {
-    console.log("*****Game over, you win*****");
-  }
-};
+buttons.forEach((btn) => btn.addEventListener("click", playRound));
 
-game();
+// this isnt working for some reason
+if (pScore == 5) {
+  console.log("*****Game over, you win*****");
+}
+if (cScore == 5) {
+  console.log("*****Game over, you lose*****");
+}
+
+////////////////////////////////////////////////////
+// const game = function () {
+//   for (i = 0; i < 5; i++) {
+//     const playerChoice = prompt('Choose between "Rock", "Paper" or "Scissors"');
+//     const computerChoice = getComputerChoice();
+//     console.log(playRound(playerChoice, computerChoice));
+//     console.log(`Current score is player: ${pScore} | Computer: ${cScore}`);
+//   }
+//   if (pScore === cScore) {
+//     console.log("*****Game over, Its a Draw!*****");
+//   } else if (pScore < cScore) {
+//     console.log(C);
+//   } else {
+//     console.log("*****Game over, you win*****");
+//   }
+// };
+
+// const game = function () {
+//   playRound();
+//   for (i = 0; i < 5; i++) {
+//     console.log(`Current score is player: ${pScore} | Computer: ${cScore}`);
+//     if (pScore === cScore) {
+//       console.log("*****Game over, Its a Draw!*****");
+//     } else if (pScore < cScore) {
+//       console.log("****Game over, you lose!*****");
+//     } else {
+//       console.log("*****Game over, you win*****");
+//     }
+//   }
+// };
+// game();
+
+// const rock = document.createElement("button");
+// rock.style.cssText = "width: 60px; height: 30px;";
+// rock.textContent = "Rock";
+// document.body.append(rock);
+
+// const scissors = document.createElement("button");
+// scissors.style.cssText = "width: 60px; height: 30px;";
+// scissors.textContent = "Scissors";
+// document.body.append(scissors);
+
+// const paper = document.createElement("button");
+// paper.style.cssText = "width: 60px; height: 30px;";
+// paper.textContent = "Paper";
+// document.body.append(paper);
